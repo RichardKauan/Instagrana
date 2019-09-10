5f96486586512ad570cd0a7d50ddf4b3646e4172
RichardKauan <richardkauan0@gmail.com>
2019-09-11 03:22:33 +0900

    Câmera funcionando e resolução quase certa

---
+++ www/components/cam.js
@@ -0,0 +1,22 @@
+  document.addEventListener("deviceready", onDeviceReady, false);
+function onDeviceReady() {
+    console.log(navigator.camera);
+    document.getElementById("cam").addEventListener 
+   ("click", cameraTakePicture);
+   function cameraTakePicture() { 
+   navigator.camera.getPicture(onSuccess, onFail, {  
+      quality: 100, 
+      destinationType: Camera.DestinationType.DATA_URL,
+      allowEdit: true,
+   });  
+   
+   function onSuccess(imageData) { 
+      var image = document.getElementById('myImage'); 
+      image.src = "data:image/PNG;base64," + imageData; 
+   }  
+   
+   function onFail(message) { 
+      alert('Failed because: ' + message); 
+   } 
+}
+}
